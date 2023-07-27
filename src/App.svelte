2fd1cc116ad63b9156d8cac9c1e3f720d7243fd9
ikhsan nur <ikhsan15nur@gmail.com>
2023-07-27 14:16:59 +0700

    loop

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,33 @@
 <script>
-	let firstName = 'ikhsan';
-	let lastName = 'nur';
-	let beltColour = 'black';
-
-	// reactive values
-	// start
-	$: fullName = `${firstName} ${lastName}`;
-	$: {
-		console.log(beltColour);
-		console.log(fullName);
-	}
-	// stop
-
-	const handleClick = () => {
-		beltColour = 'orange';
-	};
-	const handleInput = (e) => {
-		beltColour = e.target.value;
-	};
+  let orang = [
+    {nama: 'ikhsan', beltColour: 'black', usia: 35, id: 1},
+    {nama: 'aida', beltColour: 'brown', usia: 24, id: 2},
+    {nama: 'aican', beltColour: 'orange', usia: 40, id: 3}
+  ]
 </script>
 
 <main>
-	<!-- <p>{firstName} {lastName} - {beltColour} belt</p> -->
-
-	<p>{fullName} - {beltColour} belt</p>
-	<input type="text" bind:value={firstName}>
-	<input type="text" bind:value={lastName}>
-	<input type="text" bind:value={beltColour}>
+  <!-- <div>
+    <h4>{orang[0].nama}</h4>
+    <p>{orang[0].beltColour}</p>
+  </div>
+  <div>
+    <h4>{orang[1].nama}</h4>
+    <p>{orang[1].beltColour}</p>
+  </div>
+  <div>
+    <h4>{orang[2].nama}</h4>
+    <p>{orang[2].beltColour}</p>
+  </div> -->
+  
+  {#each orang as person (person.id)}
+    <div>
+      <h4>{person.nama}</h4>
+      <p>{person.usia} years old, {person.beltColour} belt.</p>
+    </div>
+    {:else}
+      <p>There are no peaplo to show ...</p>
+  {/each}
 </main>
 
 <style>
---
+++ src/p1_reactive_values.svelte
@@ -0,0 +1,52 @@
+<script>
+	let firstName = 'ikhsan';
+	let lastName = 'nur';
+	let beltColour = 'black';
+
+	// reactive values
+	// start
+	$: fullName = `${firstName} ${lastName}`;
+	$: {
+		console.log(beltColour);
+		console.log(fullName);
+	}
+	// stop
+
+	const handleClick = () => {
+		beltColour = 'orange';
+	};
+	const handleInput = (e) => {
+		beltColour = e.target.value;
+	};
+</script>
+
+<main>
+	<!-- <p>{firstName} {lastName} - {beltColour} belt</p> -->
+
+	<p>{fullName} - {beltColour} belt</p>
+	<input type="text" bind:value={firstName}>
+	<input type="text" bind:value={lastName}>
+	<input type="text" bind:value={beltColour}>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
